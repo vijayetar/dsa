3b04478c61703dd6075f5d4139a7083f00d12a12
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-03 12:48:55 -0700

    code and testing done

---
+++ dsa/challenges/find_max_binary_tree/README.md
@@ -0,0 +1,34 @@
+# Find Max Value in a Binary Tree
+
+[Table of Contents](../../../README.md)
+
+See [solution](find_max_binary_tree.py)
+
+__PR__:
+
+## Challenge
+Write an __instance method__ called __find-maximum-value__. Without utilizing any of the built-in methods available to your language, __return the maximum value__ stored in the tree. You can assume that the values stored in the Binary Tree will be numeric.
+
+## Approach and Efficiency
+
+
+## Specifications Used
+* .editorconfig
+* .gitattributes
+* .gitignore
+
+
+## Solution
+
+
+## Checklist
+ - [x] Top-level README “Table of Contents” is updated
+ - [x] Feature tasks for this challenge are completed
+ - [x] Unit tests written and passing
+     - [x] “Happy Path” - Expected outcome
+     - [x] Expected failure
+     - [x] Edge Case (if applicable/obvious)
+ - [x] README for this challenge is complete
+     - [x] Summary, Description, Approach & Efficiency, Solution
+     - [x] Link to code
+     - [x] Picture of whiteboard
---
+++ dsa/challenges/find_max_binary_tree/find_max_binary_tree.py
@@ -0,0 +1,130 @@
+from dsa.data_structures.tree.tree import Queue
+
+class Node:
+    def __init__(self,value, left_ = None, right_ = None, next_ = None):
+        self.value = value
+        if (isinstance(left_,Node) == False and left_ != None) or (isinstance(right_,Node) == False and right_ != None):
+            raise TypeError("hey there is a problem with the value")
+        self.left = left_
+        self.right = right_
+        self.next= next_
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def add(self,value):
+        '''Takes single argument and adds argument value to the Binary Tree using breadth first traversal'''
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+            return
+        bt_queue = Queue()
+        def evaluate_q(root_node, new_node):
+            if not root_node.left:
+                root_node.left = new_node
+                return
+            bt_queue.enqueue(root_node.left)
+            if not root_node.right:
+                root_node.right = new_node
+                return
+            bt_queue.enqueue(root_node.right)
+            root_node = bt_queue.dequeue()
+            evaluate_q(root_node,new_node)
+        evaluate_q(self.root, new_node)
+
+    def breadth_first(self):
+        '''Returns list with node vlaues in breadth traversal'''
+        if not self.root:
+            return "NULL"
+        output = []
+        bt_queue = Queue()
+        def evaluate_q(root_node):
+            output.append(root_node.value)
+            if root_node.left:
+                bt_queue.enqueue(root_node.left)
+            if root_node.right:
+                bt_queue.enqueue(root_node.right)
+            if bt_queue.isEmpty():
+                return
+            root_node = bt_queue.dequeue()
+            evaluate_q(root_node)
+
+        evaluate_q(self.root)
+        return output
+
+    def find_max_value(self):
+        '''Finds maximum value in the binary tree and returns the number'''
+        if not self.root:
+            raise Exception("The tree is empty")
+        max = 0
+        def traverse(root, max):
+            if not root:
+                return None
+            if max < root.value:
+                max = root.value
+            if root.left:
+                max = traverse(root.left, max)
+            if root.right:
+                max = traverse(root.right, max)
+            return max
+        max = traverse(self.root, max)
+        return max
+
+
+
+    def preOrder(self):
+        '''Returns list with node values in depth first traversal in the preorder method'''
+        if not self.root:
+            return "NULL"
+        output = []
+        def walk(root_node):
+            output.append(root_node.value)
+            if root_node.left:
+                walk(root_node.left)
+            if root_node.right:
+                walk(root_node.right)
+        walk(self.root)
+        return output
+
+    def inOrder(self):
+        '''Returns list with node values in depth first traversal in the inorder method'''
+        if not self.root:
+            return "NULL"
+        output = []
+        def walk(root_node):
+            if root_node.left:
+                walk(root_node.left)
+            output.append(root_node.value)
+            if root_node.right:
+                walk(root_node.right)
+        walk(self.root)
+        return output
+
+    def postOrder(self):
+        '''Returns list with node values in depth first traversal in the postorder method'''
+        if not self.root:
+            return "NULL"
+        output = []
+        def walk(root_node):
+            if root_node.left:
+                walk(root_node.left)
+            if root_node.right:
+                walk(root_node.right)
+            output.append(root_node.value)
+        walk(self.root)
+        return output
+
+    def __str__(self):
+        if not self.root:
+            return "NULL"
+        self.final_string = ""
+        def bt_output(root_node):
+            self.final_string+= f"{root_node.value}--> "
+            if root_node.left:
+                bt_output(root_node.left)
+            if root_node.right:
+                bt_output(root_node.right)
+            return
+        bt_output(self.root)
+        return f"{self.final_string}NULL"
