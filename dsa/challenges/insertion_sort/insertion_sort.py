220a289bb8a8bcfc51d16f22328c000bff29de79
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-16 10:03:49 -0700

    added another function without temp variable

--- dsa/challenges/insertion_sort/insertion_sort.py
+++ dsa/challenges/insertion_sort/insertion_sort.py
@@ -1,4 +1,5 @@
 def sort_insertion(arr):
+    ''' function to do sort insertion of an array using temp variable '''
     for i in range(len(arr)):
         j = i - 1
         temp = arr[i]
@@ -6,6 +7,15 @@ def sort_insertion(arr):
             arr[j+1] = arr[j]
             j = j-1
         arr[j+1] = temp
+    return arr
+
+def sort_insertion1(arr):
+    ''' function doing sort insertion without temp, but swapping values '''
+    for i in range(len(arr)):
+        j = i - 1
+        while j>=0 and arr[j+1] < arr[j]:
+            arr[j+1], arr[j] = arr[j], arr[j+1]
+            j = j-1
     return arr
 if __name__ == "__main__":
     arr = [8,4,23,42,16,15]
