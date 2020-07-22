5f80defa13740cea33693fbc6aa99ee01c42f8c0
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-07-22 11:07:50 -0700

    2.3 completed

--- dsa/practice_coding/practice_linked_lists/README.md
+++ dsa/practice_coding/practice_linked_lists/README.md
@@ -1,8 +1,12 @@
 # Linked Lists
 ####  page 94 from breaking the coding interview
 
-#### 2.1 Remove Duplicates from unsorted linked list.  Implement it with and without a temporary buffer
-
 [Answer](practice_linked_lists.py)
 
+
+#### 2.1 Remove Duplicates from unsorted linked list.  Implement it with and without a temporary buffer
+
 #### 2.3 Find the middle node from the linked list
+Both mid and current start at the head, and for every two steps the current pointer advances, the mid pointer advances by 1
+
+#### 2.3b find the middle node and remove it from the linked list
--- dsa/practice_coding/practice_linked_lists/practice_linked_lists.py
+++ dsa/practice_coding/practice_linked_lists/practice_linked_lists.py
@@ -45,9 +45,48 @@ def remove_duplicates2(ll):
         current = current.next
     return ll
 
+# 2.3 find the middle node
+def find_middle(ll):
+    print(ll)
+    if not ll:
+        return "no linked list provided"
+    if not ll.head.next:
+        return ll.head.value
+    current = ll.head
+    mid = current
+    while current.next:
+        if current.next.next:
+            current = current.next.next
+            mid = mid.next
+        else:
+            break
+    return mid.value
+
+# 2.3a find and remove the middle node
+def delete_middle(ll):
+    if not ll:
+        return "no linked list provided"
+    current = ll.head
+    mid = current
+    prev = current
+    while current.next:
+        if current.next.next:
+            current = current.next.next
+            mid = mid.next
+            if prev.next != mid:
+                prev = prev.next
+        else:
+            break
+    prev.next = mid.next
+    mid.next = None
+    return ll
 
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert("C", "E", "A", "E", "D", "C", "A", "C", "A")
-    print(remove_duplicates2(ll))
+    ll.insert("A", "B")
+    # ll.insert("C", "E", "A", "E", "D", "C", "A", "C", "A")
+    print(ll)
+    print(delete_middle(ll))
+
+
 
