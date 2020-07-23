ac3eb63bd086886af0f83fb96934e8ac78ec4420
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-07-23 13:27:48 -0700

    q2.4 completed

--- dsa/practice_coding/practice_linked_lists/README.md
+++ dsa/practice_coding/practice_linked_lists/README.md
@@ -10,3 +10,5 @@
 Both mid and current start at the head, and for every two steps the current pointer advances, the mid pointer advances by 1
 
 #### 2.3b find the middle node and remove it from the linked list
+
+#### 2.4 Given ll, and partition value, use the partition value given and return a linked list with nodes sorted around the partition value
--- dsa/practice_coding/practice_linked_lists/practice_linked_lists.py
+++ dsa/practice_coding/practice_linked_lists/practice_linked_lists.py
@@ -1,4 +1,4 @@
-from dsa.data_structures.linked_list.linked_list import LinkedList
+from dsa.data_structures.linked_list.linked_list import LinkedList, Node
 
 #### 2.1 Remove Duplicates from unsorted linked list.  Implement it with and without a temporary buffer
 
@@ -81,12 +81,46 @@ def delete_middle(ll):
     mid.next = None
     return ll
 
+# 2.4 given ll and value, partition ll on that value
+## assumptions only integers given, including negative values,  no characters given.  and value present in the linked list
+
+def partition (ll, value):
+    # make a new linked list with a tail to hold high values and head to hold the small values
+    output_ll = LinkedList()
+    output_ll.tail = output_ll.head
+    current = ll.head
+    while current:
+        if current.value < value:
+            if not output_ll.head:
+                output_ll.head = Node(current.value)
+                output_ll.tail = output_ll.head
+            else:
+                new_node = Node(current.value)
+                new_node.next = output_ll.head
+                output_ll.head = new_node
+        else:
+            #add to the tail
+            if not output_ll.tail:
+                output_ll.tail = Node(current.value)
+                output_ll.head = output_ll.tail
+            else:
+                new_node = Node(current.value)
+                output_ll.tail.next = new_node
+                output_ll.tail = new_node
+        current = current.next
+    return output_ll
+
+
+
+
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert("A", "B")
+    # ll.insert("A", "B")
     # ll.insert("C", "E", "A", "E", "D", "C", "A", "C", "A")
-    print(ll)
-    print(delete_middle(ll))
+    # print(delete_middle(ll))
+    ll.insert(1, 2, 8, 3, 5, -1,200)
+    print("this is the ll", ll)
+    print(partition(ll, 3))
 
 
 
