ac70f4b0b5ba9c48dd8b6795f1e5782090ca64b2
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-29 10:19:24 -0700

    working on Stack testing

--- dsa/java_challenges/src/main/java/data_structures/stacksAndQueues/Node.java
+++ dsa/java_challenges/src/main/java/data_structures/stacksAndQueues/Node.java
@@ -1,17 +1,23 @@
 package data_structures.stacksAndQueues;
 
 public class Node {
-    public int value;
+    public Integer value;
     public Node next;
 
-    public Node(int value){
+    public Node(Integer value){
         this.value = value;
     }
-    public Node(int value, Node next){
+    public Node(Integer value, Node next){
         this.value = value;
         this.next = next;
     }
+    public Node(){
+        this.value = null;
+    }
     public String toString(){
         return String.format("{%s}",this.value);
+    }
+    public Integer getValue(){
+        return this.value;
     }
 }
--- dsa/java_challenges/src/main/java/data_structures/stacksAndQueues/Stack.java
+++ dsa/java_challenges/src/main/java/data_structures/stacksAndQueues/Stack.java
@@ -2,24 +2,7 @@ package data_structures.stacksAndQueues;
 
 import data_structures.linkedList.Node;
 
-//public class Stack<T> {
-//    private Node<T> top = null;
-//    public boolean isEmpty () { return top == null; }
-//    public T peek() { return isEmpty() ? null : top.value; }
-//    public void push(T value) {
-//        Node<T> node = new Node(value);
-//        if (!isEmpty())
-//            node.next = top;
-//        top = node;
-//    }
-//    public T pop() {
-//        if (isEmpty()) { return null; }
-//        Node<T> node = top;
-//        top = top.next;
-//        return node.value;
-//    }
-//}
-public class Stack(){
+public class Stack{
     private Node top;
 
     public Stack(Node top) {
@@ -32,6 +15,10 @@ public class Stack(){
     public boolean isEmpty(){
         return top==null;
     }
+    // get Top of the Stack
+    public Node getTop(){
+        return top;
+    }
     // returns the value of the top of the Stack
     public int peek() throws Exception {
         if (top==null){
@@ -39,12 +26,38 @@ public class Stack(){
         }
         return top.value;
     }
-    // add values to the top of the stack
-//    public void push(int val){
-//        Node newVal = new Node(val);
-//        if (top == null){
-//
-//        }
-//    }
+//     add values to the top of the stack
+    public void push(Integer val){
+        if (isEmpty()){
+            top = new Node(val);
+            return;
+        }
+        top = new Node (val, top);
+    }
+    public void push (int[] values){
+        int i = 0;
+        if (isEmpty()){
+            top = new Node(values[i], top);
+            i = 1;
+        }
+        for (int j = i; j<values.length; j++){
+            top = new Node(values[j], top);
+        }
+    }
+    // remove the top of the Stack
+    public Integer pop(){
+        Node temp = top;
+        top = top.next;
+        return temp.value;
+    }
+    public String toString(){
+        String output = "";
+        Node current = top;
+        while(current != null){
+            output += String.format("{%d}=> ", current.value);
+            current = current.next;
+        }
+        return output+"NULL";
+    }
 
 }
