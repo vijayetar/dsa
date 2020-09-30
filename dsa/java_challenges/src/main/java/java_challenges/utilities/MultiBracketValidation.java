4ab198ae661e627c1aa7d729a3166206b71a9df1
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-30 14:07:55 -0700

    started code challenges

---
+++ dsa/java_challenges/src/main/java/java_challenges/utilities/MultiBracketValidation.java
@@ -0,0 +1,7 @@
+package java_challenges.utilities;
+
+public class MultiBracketValidation {
+    //make a stack and then check each bracket and then close it and pop it off
+    // and if it is empty then we will return true
+
+}
---
+++ dsa/java_challenges/src/test/java/java_challenges/utilities/MultiBracketValidationTest.java
@@ -0,0 +1,10 @@
+package java_challenges.utilities;
+
+public class MultiBracketValidationTest {
+    public static boolean multiBracketValidation(String input){
+        //create a hashmap of the brackets with O1 look up time
+        // instantiate a stack
+        // iterate over the string and then add to the stack as we see 
+        return true;
+    }
+}
